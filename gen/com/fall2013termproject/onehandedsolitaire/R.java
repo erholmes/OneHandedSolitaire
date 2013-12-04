050e1738e7dc93757f7b7674e1ffe7e5bf2c6f8a
erholmes <rachel@Erins-MacBook-Pro.local>
2013-12-03 21:19:18 -0500

    interface changes
    updated background

--- gen/com/fall2013termproject/onehandedsolitaire/R.java
+++ gen/com/fall2013termproject/onehandedsolitaire/R.java
@@ -110,11 +110,11 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080007;
-        public static final int button1=0x7f080003;
+        public static final int button1=0x7f080002;
         public static final int button2=0x7f080004;
         public static final int cardImage=0x7f080001;
         public static final int drawButton=0x7f080000;
-        public static final int imageView1=0x7f080002;
+        public static final int imageView1=0x7f080003;
         public static final int menu_settings=0x7f080006;
         public static final int textView1=0x7f080005;
     }
@@ -139,6 +139,7 @@ public final class R {
         public static final int menu_settings=0x7f050002;
         public static final int new_game=0x7f050006;
         public static final int title_activity_main=0x7f050004;
+        public static final int todo=0x7f05000b;
         public static final int view_help_activity=0x7f050009;
     }
     public static final class style {
