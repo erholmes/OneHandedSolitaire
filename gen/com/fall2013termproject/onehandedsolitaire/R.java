63c94cb60a378dfd34794eecb6a1eca0c1590f64
erholmes <rachel@Erins-MacBook-Pro.local>
2013-12-10 19:31:02 -0500

    updated layout for higher/lower game
    Updated activity card draw layout and card draw activity code to
    support the higher/lower guessing game.

--- gen/com/fall2013termproject/onehandedsolitaire/R.java
+++ gen/com/fall2013termproject/onehandedsolitaire/R.java
@@ -109,14 +109,15 @@ public final class R {
         public static final int spadesq2=0x7f020055;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int button1=0x7f080002;
-        public static final int button2=0x7f080004;
-        public static final int cardImage=0x7f080001;
-        public static final int drawButton=0x7f080000;
-        public static final int imageView1=0x7f080003;
-        public static final int menu_settings=0x7f080006;
-        public static final int textView1=0x7f080005;
+        public static final int action_settings=0x7f080008;
+        public static final int button1=0x7f080003;
+        public static final int button2=0x7f080005;
+        public static final int cardImage=0x7f080000;
+        public static final int guessHigher=0x7f080001;
+        public static final int guessLower=0x7f080002;
+        public static final int imageView1=0x7f080004;
+        public static final int menu_settings=0x7f080007;
+        public static final int textView1=0x7f080006;
     }
     public static final class layout {
         public static final int activity_card_draw=0x7f030000;
@@ -133,14 +134,16 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int card_draw_activity=0x7f050008;
         public static final int draw=0x7f050003;
+        public static final int guess_higher_card=0x7f050009;
+        public static final int guess_lower_card=0x7f05000a;
         public static final int hello_world=0x7f050001;
         public static final int help=0x7f050007;
-        public static final int help_page=0x7f05000a;
+        public static final int help_page=0x7f05000c;
         public static final int menu_settings=0x7f050002;
         public static final int new_game=0x7f050006;
         public static final int title_activity_main=0x7f050004;
-        public static final int todo=0x7f05000b;
-        public static final int view_help_activity=0x7f050009;
+        public static final int todo=0x7f05000d;
+        public static final int view_help_activity=0x7f05000b;
     }
     public static final class style {
         /** 
--- src/com/fall2013termproject/onehandedsolitaire/CardDrawActivity.java
+++ src/com/fall2013termproject/onehandedsolitaire/CardDrawActivity.java
@@ -15,97 +15,113 @@ public class CardDrawActivity extends Activity implements OnClickListener {
 	Intent newgame = getIntent();
 	
 	// UI components
-	private Button drawButton;
+	private Button guessHigher;
+	private Button guessLower;
 	private ImageView cardImage;
+	
+	
 	
 	// Random object
 	private final static Random random = new Random();
 	
 	// The card deck
-	private final static int[] cardDeck = new int[] {	R.drawable.clubsa,	// Ace through King of clubs, spades, hearts and diamonds
-														R.drawable.clubs2,	
-														R.drawable.clubs3,	
-														R.drawable.clubs4,	
-														R.drawable.clubs5,	
-														R.drawable.clubs6,
-														R.drawable.clubs7,
-														R.drawable.clubs8,
-														R.drawable.clubs9,
-														R.drawable.clubs10,
-														R.drawable.clubsj,	
-														R.drawable.clubsq,	
-														R.drawable.clubsk,
-														R.drawable.spadesa,
+	private final static int[] cardDeck = new int[] {	R.drawable.clubs2, // 2 through Ace of clubs, spades, hearts and diamonds
 														R.drawable.spades2,
-														R.drawable.spades3,
-														R.drawable.spades4,
-														R.drawable.spades5,
-														R.drawable.spades6,
-														R.drawable.spades7,
-														R.drawable.spades8,
-														R.drawable.spades9,
-														R.drawable.spades10,
-														R.drawable.spadesj,
-														R.drawable.spadesq,
-														R.drawable.spadesk,
-														R.drawable.heartsa,
 														R.drawable.hearts2,
-														R.drawable.hearts3,
-														R.drawable.hearts4,
-														R.drawable.hearts5,
-														R.drawable.hearts6,
-														R.drawable.hearts7,
-														R.drawable.hearts8,
-														R.drawable.hearts9,
-														R.drawable.hearts10,
-														R.drawable.heartsj,
-														R.drawable.heartsq,
-														R.drawable.heartsk,
-														R.drawable.diamondsa,
 														R.drawable.diamonds2,
+														R.drawable.clubs3,
+														R.drawable.spades3,
+														R.drawable.hearts3,
 														R.drawable.diamonds3,
+														R.drawable.clubs4,
+														R.drawable.spades4,
+														R.drawable.hearts4,
 														R.drawable.diamonds4,
+														R.drawable.clubs5,	
+														R.drawable.spades5,
+														R.drawable.hearts5,
 														R.drawable.diamonds5,
+														R.drawable.clubs6,
+														R.drawable.spades6,
+														R.drawable.hearts6,
 														R.drawable.diamonds6,
+														R.drawable.clubs7,
+														R.drawable.spades7,
+														R.drawable.hearts7,
 														R.drawable.diamonds7,
+														R.drawable.clubs8,
+														R.drawable.spades8,
+														R.drawable.hearts8,
 														R.drawable.diamonds8,
+														R.drawable.clubs9,
+														R.drawable.spades9,
+														R.drawable.hearts9,
 														R.drawable.diamonds9,
+														R.drawable.clubs10,
+														R.drawable.spades10,
+														R.drawable.hearts10,
 														R.drawable.diamonds10,
+														R.drawable.clubsj,
+														R.drawable.spadesj,
+														R.drawable.heartsj,
 														R.drawable.diamondsj,
+														R.drawable.clubsq,
+														R.drawable.spadesq,
+														R.drawable.heartsq,
 														R.drawable.diamondsq,
-														R.drawable.diamondsk};	
+														R.drawable.clubsk,
+														R.drawable.spadesk,
+														R.drawable.heartsk,
+														R.drawable.diamondsk,
+														R.drawable.clubsa,
+														R.drawable.spadesa,
+														R.drawable.heartsa,
+														R.drawable.diamondsa};	
 	
-	private int cardsuit1;
-	private int cardrank1;
-	private int cardsuit2;
-	private int cardrank2;
-	private int cardsuit3;
-	private int cardrank3;
-	private int cardsuit4;
-	private int cardrank4;
+	//variable to set the card value
+	private int cardValue = -1;
+	private int previousCardValue = -1;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_card_draw);
 		
-		drawButton 	= (Button)findViewById(R.id.drawButton);
+	    guessHigher = (Button)findViewById(R.id.guessHigher);
+		guessLower	= (Button)findViewById(R.id.guessLower);
 		cardImage	= (ImageView)findViewById(R.id.cardImage);
 		
-		drawButton.setOnClickListener(this);
-		
-	}
+		guessHigher.setOnClickListener(this);
+		guessLower.setOnClickListener(this);
+		//drawButton.setOnClickListener(this);	
+	} //end onCreate
 
 	@Override
 	public void onClick(View v) {
 		drawCard();
-	}
+		compareCardValue(cardValue, previousCardValue);
+		
+	} //end onClick
 
-	private void drawCard() {
+	private int drawCard() {
 		// Set the image resource to the one being taken randomly from the cardDeck resources table.
 		cardImage.setImageResource(cardDeck[random.nextInt(cardDeck.length)]);
-		
-		
-	}
+		// if this is NOT the first card drawn, set the current card value to the previous card value before
+		// resetting current card value
+        if (cardValue != -1){
+        	previousCardValue = cardValue;
+        }
+		cardValue = cardDeck[random.nextInt(cardDeck.length)];
+		return cardValue;
+	} //end drawCard
 	
-}
+	private void compareCardValue(int cardValue, int previousCardValue){
+	   // If this is not the first draw, compare current card value with previous card value
+	   if ((previousCardValue != -1) && (cardValue != -1)){
+		   
+	   }
+	} //end CompareCardValue
+	
+} //end of CardDrawActivity
+
+
